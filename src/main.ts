dff9f539694db8458ead6ffafbd221b18a3c27c3
GGDuck <vunguyenminhduc1410@gmail.com>
2025-04-10 00:30:26 +0700

    Update connect mysql for build docker

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.FRONTEND_URL,
+    origin: "*",
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true
   });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
